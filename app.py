f293fb68d1a8d0b83c01928e86b883250b88652e
hoanghm <hoanghm4@gmail.com>
2020-09-25 00:47:43 -0400

    add migration

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
 
 # config
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "asecretkey"
     # SECRET_KEY = 'default'
-    SQLALCHEMY_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     MAIL_SERVER = 'smtp.gmail.com'
@@ -39,8 +41,7 @@ db.init_app(app)
 bootstrap.init_app(app)
 login_manager.init_app(app)
 
-
-from main.models import User, AnonymousUser, Role
+from main.models import User, AnonymousUser
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'page.login'
 login_manager.anonymous_user = AnonymousUser
@@ -48,6 +49,21 @@ login_manager.anonymous_user = AnonymousUser
 def load_user(id):
     return User.query.get(id)
 
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+def make_shell_context():
+    return dict(app=app, db=db)
+manager.add_command("shell", Shell(make_context=make_shell_context()))
+manager.add_command("db", MigrateCommand)
+
+@manager.command
+def deploy():
+    from flask_migrate import upgrade
+    from main.models import User, Role
+    upgrade()
+    Role.insert_roles()
+    User.insert_admin()
 
 '''
 connect with views
@@ -57,7 +73,4 @@ app.register_blueprint(page_blueprint)
 
 
 if __name__ == '__main__':
-    db.create_all()
-    Role.insert_roles()
-    User.insert_admin()
-    app.run()
+    manager.run()
